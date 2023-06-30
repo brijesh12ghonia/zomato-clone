7352f1c01d2d018850be2c04080863d8bd122e01
brijesh987 <brijeshghonia987@gmail.com>
2023-06-30 16:32:10 +0530

    Completed home page navbar and food tab

---
+++ client/src/pages/Home.page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//layouts
+import HomepageLayout from "../layouts/Homepage.layout";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  );
+};
+
+export default HomepageLayout(Home);
